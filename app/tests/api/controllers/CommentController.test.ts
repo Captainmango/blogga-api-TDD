10e0bd01f958716fdfe972e0008c555d5a6fb42f
Edward Heaver <edward.heaver01@gmail.com>
2022-04-10 13:35:59 +0100

    changed tests a bit

--- app/tests/api/controllers/CommentController.test.ts
+++ app/tests/api/controllers/CommentController.test.ts
@@ -26,17 +26,11 @@ beforeEach(async () => {
 describe("COMMENTS API ENDPOINTS", () => {
     it.todo("GET /comments has an index route")
 
-    it.todo("GET /comments/{comment_id} is able to retrieve a single post")
+    it.todo("GET /comments/{comment_id} is able to retrieve a single comment")
 
     it.todo("GET /comments/{comment_id} returns 404 if comment cannot be found")
 
-    it.todo("PATCH /comments/{comment_id} is able to update a post")
-
-    it.todo("PATCH /comments/{comment_id} returns 404 if comment cannot be found")
-
-    it.todo("DELETE /comments/{comment_id} is able to delete a posts")
+    it.todo("DELETE /comments/{comment_id} is able to delete a comment")
 
     it.todo("DELETE /comments/{comment_id} returns 404 if comment cannot be found")
-
-    it.todo("POST /comments is able to create a new post")
 })
