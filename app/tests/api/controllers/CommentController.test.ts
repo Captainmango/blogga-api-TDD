9127928eccc81eab507dc4e16345d8a2b2d7cf93
Edward Heaver <edward.heaver01@gmail.com>
2022-05-07 16:04:41 +0100

    starting posts tests

--- app/tests/api/controllers/CommentController.test.ts
+++ app/tests/api/controllers/CommentController.test.ts
@@ -3,7 +3,7 @@ import { Express } from 'express-serve-static-core'
 import { Comment } from '../../../src/database/entities/Comment'
 import { createServer } from '../../../src/utils/server'
 import connection, { dbEnvs } from '../../../src/utils/db'
-import { tearDownDatabase, useSeeding } from 'typeorm-seeding'
+import { factory, tearDownDatabase, useSeeding } from 'typeorm-seeding'
 
 let server: Express
 
@@ -23,7 +23,11 @@ beforeEach(async () => {
 })
 
 describe("COMMENTS API ENDPOINTS", () => {
-    it.todo("GET /comments has an index route")
+    it.todo("GET /comments has an index route", async () => {
+        const comments: Comment[] = await factory(Comment)().createMany(4)
+        const comment = comments[0]
+        const {createdAt, updatedAt, ...expected} = comment
+    })
 
     it.todo("GET /comments/{comment_id} is able to retrieve a single comment")
 
